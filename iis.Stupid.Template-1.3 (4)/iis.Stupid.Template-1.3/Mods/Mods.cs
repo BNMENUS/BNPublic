a813c870153d14435a95f100d690039f2be362fb
Nobross <nilspike2@gmail.com>
2025-01-02 17:15:41 -0600

    added speed boost

---
+++ iis.Stupid.Template-1.3 (4)/iis.Stupid.Template-1.3/Mods/Mods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StupidTemplate.Mods
+{
+    internal class Mods
+    {
+        public static void MosaSpeed()
+        {
+            GorillaLocomotion.Player.Instance.maxJumpSpeed = 7.5f;
+            GorillaLocomotion.Player.Instance.jumpMultiplier = 3.5f;
+        }
+    }
+}
---
+++ iis.Stupid.Template-1.3 (4)/iis.Stupid.Template-1.3/PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.BN.gorillatag.menutemplate";
+        public const string Name = "BN MENU";
+        public const string Description = "Created by @burntpineapples and @freakynewkid with skibidi in mind";
+        public const string Version = "1.0.0";
+    }
+}
