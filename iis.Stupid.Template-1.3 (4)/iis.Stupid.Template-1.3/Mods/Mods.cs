ad191bdb73f4a4c331e0fed910d49820553da780
Nobross <nilspike2@gmail.com>
2025-01-02 18:29:55 -0600

    Added a lot of mods

--- iis.Stupid.Template-1.3 (4)/iis.Stupid.Template-1.3/Mods/Mods.cs
+++ iis.Stupid.Template-1.3 (4)/iis.Stupid.Template-1.3/Mods/Mods.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Photon.Pun;
+using Photon.Realtime;
+using StupidTemplate.Notifications;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 
 namespace StupidTemplate.Mods
@@ -12,5 +16,80 @@ namespace StupidTemplate.Mods
             GorillaLocomotion.Player.Instance.maxJumpSpeed = 7.5f;
             GorillaLocomotion.Player.Instance.jumpMultiplier = 3.5f;
         }
-    }
+
+        public static void LongArms()
+        {
+            GorillaLocomotion.Player.Instance.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        }
+
+        public static void ReallyLongArms()
+        {
+            GorillaLocomotion.Player.Instance.transform.localScale = new Vector3(2f, 2f, 2f);
+        }
+
+        public static void WaterBalloonSpammer()
+        {
+            if (ControllerInputPoller.instance.rightGrab)
+            {
+                UnityEngine.Object.Destroy(GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R/palm.01.R/TransferrableItemRightHand/SnowballRightAnchor").transform.Find("LMACF.").GetComponent<AudioSource>());
+                GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R/palm.01.R/TransferrableItemRightHand/SnowballRightAnchor").transform.Find("LMACF.").GetComponent<SnowballThrowable>().projectilePrefab.tag = "WaterBalloonProjectile";
+                GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R/palm.01.R/TransferrableItemRightHand/SnowballRightAnchor").transform.Find("LMACF.").GetComponent<SnowballThrowable>().randomizeColor = false;
+                return;
+            }
+            if (!GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R/palm.01.R/TransferrableItemRightHand/SnowballRightAnchor").transform.Find("LMACF.").gameObject.GetComponent<AudioSource>())
+            {
+                GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player/rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R/palm.01.R/TransferrableItemRightHand/SnowballRightAnchor").transform.Find("LMACF.").gameObject.AddComponent<AudioSource>();
+            }
+        }
+
+
+        private static GameObject CreatePlatformOnHand(Transform handTransform)
+        {
+            GameObject plat = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            plat.transform.localScale = new Vector3(0.025f, 0.3f, 0.4f);
+
+            plat.transform.position = handTransform.position;
+            plat.transform.rotation = handTransform.rotation;
+
+            float h = (Time.frameCount / 180f) % 1f;
+            plat.GetComponent<Renderer>().material.color = Color.blue;
+            return plat;
+        }
+
+        public static void UntagAll()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                NotifiLib.SendNotification("<color=grey>[</color><color=red>ERROR</color><color=grey>]</color> <color=white>You are not master client.</color>");
+            }
+            else
+            {
+                foreach (Photon.Realtime.Player v in PhotonNetwork.PlayerList)
+                {
+                    RemoveInfected(v);
+                }
+            }
+        }
+
+        private static void RemoveInfected(Player v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void GhostMonkey()
+        {
+            bool Primary = ControllerInputPoller.instance.rightControllerPrimaryButton;
+            {
+                if (Primary == true)
+                {
+                    GorillaTagger.Instance.offlineVRRig.enabled = false;
+                }
+                else
+                {
+                    GorillaTagger.Instance.offlineVRRig.enabled = true;
+                }
+            }
+        }
+
+        }
 }
